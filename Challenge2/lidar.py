4ed78c584191c0077a1fa596306de8aea2417b5d
NVIDIA <tannerdouglas0256@flpoly.org>
2018-03-19 14:49:29 -0400

    Added PID funtionality. Works for Challenge 2. Could use some tuning.

--- Challenge2/lidar.py
+++ Challenge2/lidar.py
@@ -1,14 +1,80 @@
 import rospy as rp
 from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+
+pub = rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size = 10)
+steeringErrors = [0]
+
+def drive(speed, steering):
+	drive_msg_stamped = AckermannDriveStamped()
+	drive_msg = AckermannDrive()
+       	drive_msg.speed = speed
+        drive_msg.steering_angle = steering
+        drive_msg.acceleration = 0
+        drive_msg.jerk = 0
+        drive_msg.steering_angle_velocity = 0
+	drive_msg_stamped.drive = drive_msg
+	pub.publish(drive_msg_stamped)
+
+def getOutput(error):
+	global steeringErrors
+
+	# PID Constants
+	kP = 5.0		#0.2
+	kI = 0.0	#0.000000001
+	kD = 2.0		#2.5
+
+
+	# Calculate new PID #PleaseWork!!!
+	while(True):
+		integral = sum(steeringErrors)
+		#error = 145 - angle
+		integral = integral + error
+		if(error == 0):
+			integral = 0
+		if(abs(error) > 40):
+			integral = 0
+		derivative = error - steeringErrors[-2]
+		steeringErrors += [error]
+		turn = (kP * error) + (kI * integral) + (kD * derivative)
+		# Result can only be between -1 and 1
+		if(turn > 1):
+			turn = 1
+		elif(turn < -1):
+			turn = -1
+		return turn
+
 
 def callback(data):
+	global steeringErrors
+
 	print ("CENTER", data.ranges[540])
 	print ("Left Angle", data.ranges[780])
 	print ("Right Angle", data.ranges[300])
-	print ("Hard Left", data.ranges[940]
+	print ("Hard Left", data.ranges[940])
 	print ("Hard Right", data.ranges[140])
 
-	print ("Offset ", data.ranges[940] -  data.ranges[140])
+	print ("Offset ", data.ranges[780] - data.ranges[300])
+
+	if(data.ranges[540]<0.2):
+		speed = 0.0
+	else:
+		speed = 0.8
+	error = data.ranges[780] - data.ranges[300]
+
+	# Add to error array
+	steeringErrors += [error]
+	
+	# Only store previous 100 errors
+	if(len(steeringErrors)>100):
+		steeringErrors = steeringErrors[1:]
+	
+	# Send Error to PID
+	result = getOutput(error)
+	print("Steering: " + str(result))
+
+	# Send Message with speed and angle
+	drive(speed,result)
 
 def listener():
 	rp.init_node("laser_listener", anonymous = True)
