00a42c96dfa690f70a8f417c78eb50d43acabe17
NVIDIA <tannerdouglas0256@flpoly.org>
2018-03-23 13:04:37 -0400

    Added functionality to track object closest to left and right sides. Object is printed in meters away at angle measured from center of vehicle

--- Challenge3/lidar.py
+++ Challenge3/lidar.py
@@ -1,40 +1,39 @@
+import rospy as rp
+import numpy as np
+from sensor_msgs.msg import LaserScan
+
 def callback(data):
-	global steeringErrors
 
-	print ("CENTER", data.ranges[540])
-	print ("Left Angle", data.ranges[780])
-	print ("Right Angle", data.ranges[300])
-	print ("Hard Left", data.ranges[940])
-	print ("Hard Right", data.ranges[140])
+	#print ("", data)
+	#print ("CENTER", data.ranges[540])
+	#print ("Left Angle", data.ranges[780])
+	#print ("Right Angle", data.ranges[300])
+	#print ("Hard Left", data.ranges[940])
+	#print ("Hard Right", data.ranges[140])
 
-	print ("Offset ", data.ranges[780] - data.ranges[300])
+	#print ("Offset ", data.ranges[780] - data.ranges[300])
 
 	#Calculate how for of center we are (meters)	
-	error = data.ranges[780] - data.ranges[300]
-
-	# Add to error array
-	steeringErrors += [error]
+	#error = data.ranges[780] - data.ranges[300]
 	
-	# Only store previous 100 errors
-	if(len(steeringErrors)>100):
-		steeringErrors = steeringErrors[1:]
+	#Define lists for left and right side of lidar/car	
+	laser_right_temp = []
+	laser_right = []
+	laser_left = []
 	
-	# Send Error to PID
-	result = getOutput(error)
-	print("Steering: " + str(result))
+	#Append data from lidar to lists
+	laser_right_temp += data.ranges[20:540]
+	laser_left += data.ranges[541:1060]
+	
+	#Reverse list so that right side is measured from center
+	laser_right += laser_right_temp[::-1]
 
-	#Determine speed
-	#If center distance is greater than 0.4 meters
-	if(data.ranges[540]>0.4):
-		#speed is absolute value of 1 minus turning angle
-		speed = 1 - abs(result)
-		#if speed is less than 0.3 set speed to 0.3 as hard turns will set speed to 0.0
-		if(speed <0.6):
-			speed = 0.6
-	else:
-		speed = data.ranges[540] - 0.2
-	# Send Message with speed and angle
-	drive(speed,result)
+	#DEBUG (DELETE WHEN DONE)
+	print("Lowest Right value: ", np.amin(laser_right) , "at ", (laser_right.index(np.amin(laser_right))/4), "degrees off center")
+	print("Lowest Left value:  ", np.amin(laser_left) , "at ", (laser_left.index(np.amin(laser_left))/4), "degrees off center")
+
+	
+
 
 def listener():
 	rp.init_node("laser_listener", anonymous = True)
