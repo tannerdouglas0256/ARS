5b6744693d30b36d9d4aa42b8c3e4df08fd5932e
NVIDIA <tannerdouglas0256@flpoly.org>
2018-03-31 22:33:30 -0400

    Added functionality to steer parallel to two cones

--- Challenge3/lidar.py
+++ Challenge3/lidar.py
@@ -1,13 +1,40 @@
+import cv2
 import rospy as rp
 import numpy as np
 import math
+from sensor_msgs.msg import Image
 from sensor_msgs.msg import LaserScan
 from cv_bridge import CvBridge, CvBridgeError
-
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 bridge = CvBridge()
 
-def callback(data):
+pub = rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size = 10)
+image_right = []
 
+#Determines what portion of the course we are in
+# 0 = Initial Alignment
+# 1 = Parallel
+# 2 = Weave
+state = 0
+
+def drive(speed, steering):
+	drive_msg_stamped = AckermannDriveStamped()
+	drive_msg = AckermannDrive()
+       	drive_msg.speed = speed
+        drive_msg.steering_angle = steering
+        drive_msg.acceleration = 0
+        drive_msg.jerk = 0
+        drive_msg.steering_angle_velocity = 0
+	drive_msg_stamped.drive = drive_msg
+	pub.publish(drive_msg_stamped)
+
+def zedCam(data):
+	image_right = bridge.imgmsg_to_cv2(data, desired_encoding = "passthrough")
+	cv2.imshow("", image_right)
+	cv2.waitKey(1)
+
+def parallel(data):
+	#Parallels cones
 	#Define lists for left and right side of lidar/car	
 	laser_right_temp = []
 	laser_right = []
@@ -27,23 +54,64 @@ def callback(data):
 	left_closest_dist = np.amin(laser_left)
 	left_closest_angle = laser_left.index(np.amin(laser_left))/4
 	left_angle_offset = 90 - left_closest_angle
+
+	#Find next closest object on left side
+	laser_left_second = []
+	laser_left_second += laser_left[0:(left_closest_angle)]
+	print("Second Left value:  ", np.amin(laser_left_second) , "at ", (laser_left_second.index(np.amin(laser_left_second))/4), "degrees off center")
+	left_second_dist = np.amin(laser_left_second)
+	left_second_angle = laser_left_second.index(np.amin(laser_left_second))/4
+	left_second_offset = 90 - left_second_angle
 	
 	#Position on LEFT
-	left_y = left_closest_dist * math.sin(math.radians(left_angle_offset))
-	temp_left_x = left_closest_dist * math.cos(math.radians(left_angle_offset))
+	left_y = left_closest_dist * math.sin(math.radians(left_angle_offset/4))
+	temp_left_x = left_closest_dist * math.cos(math.radians(left_angle_offset/4))
 	left_x = temp_left_x - 0.635
 	print("LEFT Y ", left_y)
 	print("LEFT X ", left_x)
 
-	data = [left_y, left_x]
+	#Next position on left
+	left_y_next = left_second_dist * math.sin(math.radians(left_second_offset/4))
+	left_x_next_temp = left_second_dist * math.cos(math.radians(left_second_offset/4))
+	left_x_next = left_x_next_temp - 0.635
+	print("Left Y Next ", left_y_next)
+	print("Left X Next ", left_x_next)
+
+
+	if(left_y <= 0):	#if object is beyond 90 degrees on left side
+		drive(0.5, 1)
+	else:			#drive normally
+		drive(0.5, left_x)
 	
-	pub.publish(data)
+	#Find difference in x distances of both objects
+	new_x = left_x + left_x_next
+
+	drive(0.5, new_x)
+
+
+def weave(lidar):
+	#Weave thru cones
+	x =1
+def lidar(data):
+
+	parallel(data)	
 	
+#	if(left_y == 0):
+#		if(left_x == 0):
+#			#go to next operation
+#			state = 1
+#		if(left_x != 0):
+#			#GO BACK AND RE-ATTEMPT
+#			drive(-2, 0)
+#	if(left_y > 0):
+#		#DRIVE FORWARD
+#		drive(left_y, left_x)
 	
 
 def listener():
-	rp.init_node("laser_listener", anonymous = True)
-	rp.Subscriber("/scan", LaserScan, callback)
+	rp.init_node("Challenge3", anonymous = True)
+	rp.Subscriber("zedRight", Image, zedCam)
+	rp.Subscriber("/scan", LaserScan, lidar)
 	rp.spin()
 	
 if __name__ =="__main__":
