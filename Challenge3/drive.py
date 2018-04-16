bb141f2c9050b2b6ed16c9644e485c12b23f5261
NVIDIA <tannerdouglas0256@flpoly.org>
2018-04-16 01:15:53 -0400

    Work done 4/15/18 Incorporated color tracking using zed Shortened max tracking distance with lidar

--- Challenge3/drive.py
+++ Challenge3/drive.py
@@ -1,5 +1,6 @@
 import rospy as rp
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from std_msgs.msg import String
 
 pub = rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size = 10)
 steeringErrors = [0]
@@ -7,7 +8,7 @@ steeringErrors = [0]
 def drive(speed, steering):
 	drive_msg_stamped = AckermannDriveStamped()
 	drive_msg = AckermannDrive()
-       	drive_msg.speed = speed
+       	drive_msg.speed = 1.0
         drive_msg.steering_angle = steering
         drive_msg.acceleration = 0
         drive_msg.jerk = 0
@@ -18,11 +19,10 @@ def drive(speed, steering):
 def getOutput(error):
 	global steeringErrors
 
-	print ("ERROR", error)
 	# PID Constants
 	kP = 0.7		#0.2
 	kI = 0.0	#0.000000001
-	kD = 0		#2.5
+	kD = 0.0		#2.5
 
 
 	# Calculate new PID #PleaseWork!!!
@@ -36,30 +36,55 @@ def getOutput(error):
 			integral = 0
 		derivative = error - steeringErrors[-2]
 		steeringErrors += [error]
-		turn = (kP * error) + (kI * integral) + (kD * derivative)
+		speed = (kP * error) + (kI * integral) + (kD * derivative)
 		# Result can only be between -1 and 1
-		if(turn > 1):
-			turn = 1
-		elif(turn < -1):
-			turn = -1
-		return turn
+		if(speed > 1):
+			speed = 1
+		elif(speed < -1):
+			speed = -1
+		return speed
 
-def callback(data):
+	# Calculate PID
+	#proportional = error
+	#integral = sum(steeringErrors)
+	#derivative = error - steeringErrors[-2]
+
+	# Get Result
+	#result = (kP * proportional) + (kI * integral) + (kD * derivative)
+
+	# Result can only be between -1 and 1
+	#if(result > 1):
+	#	result = 1
+	#elif(result < -1):
+	#	result = -1
+	#return result
+
+def followLine(data):
+	global steeringErrors
+	speed = 0.8
+	angle = float(data.data)
+	error = 300 - angle
+
+	# Add to error array
+	steeringErrors += [error]
 	
-	print("DATA: ", data)
+	# Only store previous 100 errors
+	if(len(steeringErrors)>100):
+		steeringErrors = steeringErrors[1:]
 	
-#	if(left_y == 0):
-#		if(left_x == 0):
-#			#go to next operation
-#		if(left_x != 0):
-#			#GO BACK AND RE-ATTEMPT
-#	if(left_y > 0):
-#		#DRIVE FORWARD
+	# Send Error to PID
+	result = getOutput(error)
+	print("Angle: " + str(angle))
+	print("Error: " + str(error))
+	print("Steering: " + str(getOutput(error)))
+
+	# Send Message with speed and angle
+	drive(speed,result)
 
 def listener():
-	rp.init_node("Driver", anonymous = True)
-	rp.Subscriber(#"CHANGE THIS to our lidar function publisher", LaserScan, callback)
-	#rate = rp.Rate(60)
+	rp.init_node("VESC_Steering", anonymous = False)
+	rp.Subscriber("CVOutput", String, followLine)
+	rate = rp.Rate(60)
 	rp.spin()
 	
 if __name__ =="__main__":
